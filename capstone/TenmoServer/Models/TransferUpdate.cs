658c03d3a94fc09552e8577fe5b11300bd529fe4
Darr Whitcomb <whitcombdarr@yahoo.com>
2022-11-04 15:50:30 -0400

    account update works

---
+++ capstone/TenmoServer/Models/TransferUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenmoServer.Models
+{
+    public class TransferUpdate
+    {
+
+        public int User { get; set; }
+
+        public double AmountToSend { get; set; }
+        public TransferUpdate()
+        {
+
+        }
+
+        public TransferUpdate(int user, double amountToSend)
+        {
+            this.User = user;
+            this.AmountToSend = amountToSend;
+
+        }
+    }
+}
